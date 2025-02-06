114ebb9affd643b55f5519db3c63b77659a5899a
Carmine Anderson <carmine.anderson@outlook.com>
2025-02-06 16:26:53 -0500

    Initial Commit

---
+++ MusAIc_App/MusAIc_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusAIc_AppApp.swift
+//  MusAIc_App
+//
+//  Created by Carmine Anderson on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MusAIc_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusAIc_AppTests/MusAIc_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MusAIc_AppTests.swift
+//  MusAIc_AppTests
+//
+//  Created by Carmine Anderson on 2/6/25.
+//
+
+import Testing
+
+struct MusAIc_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
